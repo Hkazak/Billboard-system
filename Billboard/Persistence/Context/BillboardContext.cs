f6a3b4c92db654520b1f83dbd1336413cc1d06a2
Abdussattar Kassymbekov <abdusattar.kasymbekov@gmail.com>
2023-09-19 02:08:25 +0600

    Add db context

---
+++ Billboard/Persistence/Context/BillboardContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Context;
+
+public class BillboardContext : DbContext
+{
+    protected BillboardContext()
+    {
+    }
+
+    public BillboardContext(DbContextOptions options) : base(options)
+    {
+    }
+}
