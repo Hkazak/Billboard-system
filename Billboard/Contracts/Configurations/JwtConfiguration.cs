741396e271d2b5f8fdfcd575bee476ed9625fbc9
Abdussattar Kassymbekov <abdusattar.kasymbekov@gmail.com>
2023-09-24 20:10:04 +0600

    Remove empty instance from jwt configuration

--- Billboard/Contracts/Configurations/JwtConfiguration.cs
+++ Billboard/Contracts/Configurations/JwtConfiguration.cs
@@ -1,18 +1,8 @@
-﻿using System.Text.Json.Serialization;
-
-namespace Contracts.Configurations;
+﻿namespace Contracts.Configurations;
 
 public record JwtConfiguration
 {
     public required string Key { get; init; }
     public required string Issuer { get; init; }
     public required string Audience { get; init; }
-
-    [JsonIgnore]
-    public static readonly JwtConfiguration Empty = new()
-    {
-        Key = string.Empty,
-        Issuer = string.Empty,
-        Audience = string.Empty
-    };
 }
