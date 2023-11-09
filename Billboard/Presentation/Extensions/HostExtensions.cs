ac7eada9642e590f602a7824aaecfadf1490302c
Abdussattar Kassymbekov <abdusattar.kasymbekov@gmail.com>
2023-11-09 21:03:55 +0600

    Added serilog with logging to local file

---
+++ Billboard/Presentation/Extensions/HostExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Serilog;
+
+namespace Presentation.Extensions;
+
+public static class HostExtensions
+{
+    public static void ConfigureSerilog(this ConfigureHostBuilder host)
+    {
+        host.UseSerilog((_, configuration) =>
+        {
+            var path = Path.Combine(AppContext.BaseDirectory, $"logs for {DateTime.UtcNow.ToString("ddMMyyyy-hhmmss")}.log");
+            configuration.WriteTo.File(path);
+        });
+    }
+}
