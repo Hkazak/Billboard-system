6b2c3ac2593e1045ecb981f4099d8872ade34abb
Hkazak <temironline@gmail.com>
2023-11-16 16:00:30 +0600

    Billboard type config added

--- Billboard/Persistence/Migrations/20231116095637_BillboardTypeAdded.Designer.cs
+++ Billboard/Persistence/Migrations/20231116095637_BillboardTypeAdded.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Persistence.Context;
@@ -11,9 +12,11 @@ using Persistence.Context;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(BillboardContext))]
-    partial class BillboardContextModelSnapshot : ModelSnapshot
+    [Migration("20231116095637_BillboardTypeAdded")]
+    partial class BillboardTypeAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Billboard/Persistence/ModelConfigurations/BillboardTypeConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Persistence.Enums;
+using Persistence.Models;
+
+namespace Persistence.ModelConfigurations;
+
+public class BillboardTypeConfiguration : IEntityTypeConfiguration<BillboardType>
+{
+    public void Configure(EntityTypeBuilder<BillboardType> builder)
+    {
+        var roles = new List<BillboardType>
+        {
+            new BillboardType
+            {
+                Id = BillboardTypeId.SingleSide, Type = BillboardTypeId.SingleSide.ToString()
+            },
+            new BillboardType
+            {
+                Id = BillboardTypeId.DoubleSide, Type = BillboardTypeId.DoubleSide.ToString()
+            },
+            new BillboardType
+            {
+                Id = BillboardTypeId.TripleSide, Type = BillboardTypeId.TripleSide.ToString()
+            }
+        };
+        builder.HasKey(e => e.Id);
+        builder.HasData(roles);
+    }
+}
