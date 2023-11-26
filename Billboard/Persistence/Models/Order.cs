9eea65b5d8dbcfaf21a9c34e41f27d2aad608168
Hkazak <temironline@gmail.com>
2023-11-26 20:12:15 +0600

    Db config for order added

--- Billboard/Persistence/Models/Order.cs
+++ Billboard/Persistence/Models/Order.cs
@@ -6,9 +6,9 @@ public class Order
 {
     public Guid Id { get; private set; } = Guid.NewGuid();
     public required decimal ProductPrice { get; init; }
-    public required GroupOfTariffs GroupOfTariffs { get; init; }
-    public required DateTime StartDate { get; init; }
-    public required DateTime EndDate { get; init; }
+    public required Billboard Billboard { get; init; }
+    public required DateTime StartDate { get; set; }
+    public required DateTime EndDate { get; set; }
     public IEnumerable<Discount> Discounts { get; set; }
     public OrderStatusId StatusId { get; init; }
     public OrderStatus? OrderStatus { get; private set; }
