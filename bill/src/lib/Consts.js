b9c423bb1033a4870cab09a015a2b7a95b08199f
yescorp <yestai777@gmail.com>
2023-11-29 23:19:46 +0600

    change base url

--- bill/src/lib/Consts.js
+++ bill/src/lib/Consts.js
@@ -1,5 +1,5 @@
-//export const baseUrl = 'https://api.devartz.kz';
-export const baseUrl = 'http://localhost:5274';
+export const baseUrl = 'https://api.devartz.kz';
+// export const baseUrl = 'http://localhost:5274';
 
 export const localStorageForgotPasswordEmail = 'forgotPassword:email';
 
